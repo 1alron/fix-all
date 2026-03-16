3f6a8cc7be399ba983dcf75d700b6903c6ff9109
1alron <roma.niko393@gmail.com>
2026-03-16 17:17:57 +0300

    Changed AuthDestinations type

--- app/src/main/java/io/alron/fixall/auth/presentation/AuthDestinations.kt
+++ app/src/main/java/io/alron/fixall/auth/presentation/AuthDestinations.kt
@@ -1,6 +1,6 @@
 package io.alron.fixall.auth.presentation
 
-sealed class AuthRoute(val route: String) {
-    data object Login : AuthRoute("login")
-    data object Registration : AuthRoute("destination")
+enum class AuthRoute(val route: String) {
+    Login("login"),
+    Registration("destination")
 }
